f12118837a8e8da0730fc8167abf1a173eb8a26e
aaronjwhowie <aaronjwhowie@gmail.com>
2023-01-10 10:47:50 -0500

    Yum

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -4,6 +4,7 @@ import Top from '../components/top.jsx'
 export default function home() {
   return (
     <div>
+      <a href = "https://master.d1ruiiiiva27xd.amplifyapp.com/"> Deployed Site</a>
               <Top/>
               <img alt = "photo" src = "./Images/best.svg"></img>
         <Slider/>
